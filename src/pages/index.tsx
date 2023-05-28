655f3662c113231849e4bf2c9d5cff5f37d4e871
sam000urai <sam000urai@gmail.com>
2023-05-28 11:29:46 +0900

    add

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
+      <h1 className="mb-4 text-green-500 text-3xl">サンプル</h1>
+      <p className="mb-2 text-center">sample text</p>
+      <button className="btn-blue">Let's Start!!</button>
+    </section >
+  )
+}
